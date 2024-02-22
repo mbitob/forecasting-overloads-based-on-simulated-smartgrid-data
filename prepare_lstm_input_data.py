cc1f650ea1f6e36d5931ed14c54221679c100dd6
Daniel Hauer <daniel.hauer@siemens.com>
2024-02-22 13:18:45 +0100

    added prepare data script, moved utils and model scripts, adapted train_lstm_quantile to new experiment folder structure + moving scaling from training script to data preperation script

---
+++ prepare_lstm_input_data.py
@@ -0,0 +1,142 @@
+import pandas as pd
+import json
+import os
+
+
+def prepare_lstm_data(settlement = "Rural-LV1-101-2034", experiment = "BaseScenario", time_horizon = "0101-3112"):
+    """
+    This function prepares the training, validation and test data for the LSTM model for three different targets: LV, EC, NP.
+    It reads the raw data from the BIFROST experiment, scales the data and saves it to the correct path.
+    Additionally, it saves the scaling factors for each target to a json file (scaling is done with Min-Max normalization of training data).
+    """
+
+    experiment_main_path = os.path.join('./experiments/',settlement,experiment,time_horizon)
+
+    experiment_data_path = os.path.join(experiment_main_path,"data")
+    lstm_path = os.path.join(experiment_main_path,"analysis","lstm","data")
+
+    if os.path.exists(lstm_path)==False:
+        os.makedirs(lstm_path) 
+
+    measurement_data_path = f'{experiment_data_path}/grid_measurements.csv'
+    weather_data_path     = f'{experiment_data_path}/weather_data.csv'
+    sun_data_path         = f'{experiment_data_path}/sun_data.csv'
+
+
+    # Start dataset loading
+    df_measurements = pd.read_csv(measurement_data_path, low_memory=False, index_col=0)
+    df_weather = pd.read_csv(weather_data_path, low_memory=False, index_col=0)
+    df_sun = pd.read_csv(sun_data_path, low_memory=False, index_col=0)
+    
+    # Convert to datetime (all data is UTC time zone but sun behaves like CET time (NOT CEST))
+    df_weather.index = pd.to_datetime(df_weather.index, utc=True) 
+    df_measurements.index = pd.to_datetime(df_measurements.index, utc=True) 
+    df_sun.index = pd.to_datetime(df_sun.index, utc=True) 
+
+    # get keys for the columns in the raw data
+    altitude_key = next(filter(lambda col:'SUN-ALTITUDE' in col, df_sun.columns))
+    azimuth_key = next(filter(lambda col:'SUN-AZIMUTH' in col, df_sun.columns))
+    keys = {}
+    keys["LV"] = next(filter(lambda col:'LV-TRANSFORMER:' in col and 'ACTIVE-POWER-3P' in col, df_measurements.columns))
+    keys["EC"] = next(filter(lambda col:'EC-TOTAL-POWER' in col and 'REC:' in col, df_measurements.columns))
+    keys["NP"] = next(filter(lambda col:'LV-TRANSFORMER:' in col and 'NP-TOTAL-POWER:' in col, df_measurements.columns))
+
+    # save data for each target key
+    for key in keys.keys():
+        
+
+        # prepare pd df for each target
+        year_df = pd.DataFrame(columns=[key, 'sun_altitude', 'sun_azimuth', 'irradiance_real', 'irradiance_fc'])
+        
+        year_df[key]               = df_measurements[keys[key]] 
+        year_df['irradiance_real'] = df_weather[year_df.index[0]: year_df.index[-1]]['real']
+        year_df['irradiance_fc']   = df_weather[year_df.index[0]: year_df.index[-1]]['fc']
+        year_df['sun_altitude']    = df_sun[year_df.index[0]: year_df.index[-1]][altitude_key].values
+        year_df['sun_azimuth']     = df_sun[year_df.index[0]: year_df.index[-1]][azimuth_key].values
+
+        # SCALING of Irradiance Forecast
+        #df_combinded['irradiance_fc']  = df_combinded['irradiance_fc'] * 1.2 #ATTENTION
+
+
+        # Save first 6 months as training data
+
+        train_df =  year_df['2021-01-01':'2021-07-01']
+        train_df.to_csv(os.path.join(lstm_path,'{}_train.csv'.format(key)))
+        
+        target_columns = list(train_df.columns)
+        df_train_scaled = pd.DataFrame(columns = target_columns)
+    
+        #Min-Max normalization
+        scaling_dictionary = {}
+        for column in target_columns:
+            max_val = train_df.loc[:, column].values.max()
+            min_val = train_df.loc[:, column].values.min()
+            scaling_dictionary[column] = {'max' : max_val, 'min': min_val}
+            df_train_scaled[column] = (train_df[column].values - min_val) / (max_val - min_val)
+        df_train_scaled.index = train_df.index
+        
+        # save scaled training data
+        df_train_scaled.to_csv(os.path.join(lstm_path,'{}_scaled_train.csv'.format(key)))
+
+        # Write Scaling factors:
+        scaling_factors_path = os.path.join(lstm_path, '{}_scalings.json'.format(key))
+        with open(scaling_factors_path, 'w') as file:
+            json.dump(scaling_dictionary, file)
+
+
+        # Validation and test are split in 15 day segments for validation/testing:
+        # Rest in 15 day chunks, split equally one by one to validation/test
+
+        testvalid_df = year_df['2021-07-01':'2022-01-01']  
+
+        index = 0  
+        
+        # Iterate over each month in the testvalid_df  
+        for month in pd.date_range('2021-07-01', '2022-01-01', freq='MS'):  
+        
+            # Split the month into validation, test, and remaining segments  
+            start_date = month.strftime('%Y-%m-%d')  
+            end_date = (month + pd.Timedelta(days=14)).strftime('%Y-%m-%d')  
+            val_df = testvalid_df[start_date:end_date]  
+            
+            start_date = end_date  
+            end_date = (month + pd.Timedelta(days=29)).strftime('%Y-%m-%d')  
+            test_df = testvalid_df[start_date:end_date]  
+            
+
+            val_df.to_csv(os.path.join(lstm_path,f'{key}_val{index}.csv'))  
+            test_df.to_csv(os.path.join(lstm_path,f'{key}_test{index}.csv'))  
+        
+            # scale val and test values using the scaling factors from the training data
+            df_test_scaled = pd.DataFrame(columns = target_columns)
+            df_valid_scaled = pd.DataFrame(columns = target_columns) 
+
+            for column in target_columns:
+                max_val = scaling_dictionary[column]['max']
+                min_val = scaling_dictionary[column]['min']
+                df_valid_scaled[column] = (val_df[column].values - min_val) / (max_val - min_val)
+            df_valid_scaled.index = val_df.index
+            
+
+            for column in target_columns:
+                max_val = scaling_dictionary[column]['max']
+                min_val = scaling_dictionary[column]['min']
+                df_test_scaled[column] = (test_df[column].values - min_val) / (max_val - min_val)
+            df_test_scaled.index = test_df.index
+            
+            # save scaled validation and test data
+            df_valid_scaled.to_csv(os.path.join(lstm_path, f'{key}_scaled_val{index}.csv'))
+            df_test_scaled.to_csv(os.path.join(lstm_path, f'{key}_scaled_test{index}.csv'))
+                
+            index += 1
+    
+
+if __name__ == '__main__':
+
+    os.chdir(os.path.dirname(__file__))
+
+    settlement = "Rural-LV1-101-2034"
+    experiment = "BaseScenario"
+    time_horizon = "0101-3112"
+
+    prepare_lstm_data(settlement, experiment, time_horizon)
--- train_lstm_quantile.py
+++ train_lstm_quantile.py
@@ -1,40 +1,44 @@
 import argparse
 import json
-import os
+import os, sys
 import pandas as pd
-import numpy as np 
 import torch 
-import torch.nn as nn
 from torch.utils.data import DataLoader
 from time import time as get_time
-from utils import train_one_epoch, evaluate, generate_sequences
-from utils import SequenceDataset, QuantileLossMulti, search_csv_files
+from tools.utils import train_one_epoch, evaluate, generate_sequences
+from tools.utils import SequenceDataset, QuantileLossMulti, search_csv_files
 
-from models import simpleLSTM_quantiles
+from tools.models import simpleLSTM_quantiles
 
 torch.manual_seed(1)
 
-def main():
-    device = args.device 
-    BATCH_SIZE = args.batch_size 
-    EPOCH = args.epochs 
-    LR = args.lr 
-    lookback = args.lookback #Timestamps tp lookback for prediction
-    experiment_path = args.experiment_path
-    data_path = args.data_path
+def train_lstm_quantile():
+    
+    device  	        = args.device 
+    BATCH_SIZE          = args.batch_size 
+    EPOCH               = args.epochs 
+    LR                  = args.lr 
+    lookback            = args.lookback    #Timestamps tp lookback for prediction
+    experiment_path     = args.experiment_path
     positional_encoding = args.positional_encoding 
-    #train_val_test_split = args.train_val_test_split # Splits the data into train, valid, and test chunks
-    model_save_path =  os.path.join(experiment_path, 'best_valid_model.pt')
-    scaling_factors_path = os.path.join(experiment_path, 'scalings.json')
+    target              = args.target
+
+    start_time = get_time()
+
+    # set up paths
+    data_path           = os.path.join(experiment_path, 'analysis', 'lstm', "data")
+    model_save_path     = os.path.join(experiment_path,'analysis', 'lstm', "model")
+    os.makedirs(model_save_path, exist_ok=True)
+    scaling_factors_file = os.path.join(data_path, f'{target}_scalings.json')
+    
+    # define quantiles
     quantiles = [0.02, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.98]
     quantile_weights=[1 for i in range(len(quantiles))]
 
-    os.makedirs(experiment_path, exist_ok=True)
-  
+   
     # Dataframe for storing important metrics over epochs
-    start_time = get_time()
-    df_metric = pd.DataFrame(columns= ['train_loss', 'valid_loss', 'epoch', 'learning_rate' , 'batch_size_train', 'batch_size_valid'])
-    best_val_loss = 1e3 #Init
+    df_metric       = pd.DataFrame(columns= ['train_loss', 'valid_loss', 'epoch', 'learning_rate' , 'batch_size_train', 'batch_size_valid'])
+    best_val_loss   = 1e3 #Init
     best_train_loss = 1e3 # Init
         
     # Initialze new network
@@ -45,7 +49,6 @@ def main():
     elif positional_encoding == 'none':
         n_input = 1
         
-    #print(n_input)
     model = simpleLSTM_quantiles(n_input_features = n_input, n_hidden = 50, num_layers = 4, n_outputs = 1)
     #model = simpleCfC(n_input_features = n_input, n_hidden = 50, num_layers = 1, n_outputs = 1)
     model.to(device)
@@ -57,65 +60,21 @@ def main():
     scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=100, T_mult=1, eta_min=0)
 
     # Start dataset loading
-    df_train = pd.read_csv(os.path.join(data_path, 'train.csv'), index_col=0)
-    df_test_list = []
-    df_valid_list = []
+    df_train_scaled = pd.read_csv(os.path.join(data_path, f'{target}_scaled_train.csv'), index_col=0)
+    df_test_scaled_list = []
+    df_valid_scaled_list = []
 
-    valid_csv_files = search_csv_files(data_path, 'val')
-    test_csv_files = search_csv_files(data_path, 'test')
+    valid_csv_files = search_csv_files(data_path, f'{target}_scaled_val')
+    test_csv_files = search_csv_files(data_path, f'{target}_scaled_test')
 
     for valid_csv_path in valid_csv_files:
-        df_val = pd.read_csv(valid_csv_path, index_col=0)
-        df_valid_list.append(df_val)
+        df_val_scaled = pd.read_csv(valid_csv_path, index_col=0)
+        df_valid_scaled_list.append(df_val_scaled)
     
     for test_csv_path in test_csv_files:
-        df_test = pd.read_csv(test_csv_path, index_col=0)
-        df_test_list.append(df_test)
+        df_test_scale = pd.read_csv(test_csv_path, index_col=0)
+        df_test_scaled_list.append(df_test_scale)
 
-    target_columns = list(df_train.columns)
-    df_train_scaled = pd.DataFrame(columns = target_columns)
-    df_test_scaled_list = [pd.DataFrame(columns = target_columns) for i in range(len(test_csv_files))]
-    df_valid_scaled_list = [pd.DataFrame(columns = target_columns) for i in range(len(valid_csv_files))]
-    
-    #Min-Max normalization
-    scaling_dictionary = {}
-    for column in target_columns:
-        max_val = df_train.loc[:, column].values.max()
-        min_val = df_train.loc[:, column].values.min()
-        #scaling_dictionary[column] = {'min' : min_val}
-        scaling_dictionary[column] = {'max' : max_val, 'min': min_val}
-        #print(df_train_val[column])
-        df_train_scaled[column] = (df_train[column].values - min_val) / (max_val - min_val)
-    df_train_scaled.index = df_train.index
-        
-    for i, (df_valid, df_valid_scaled) in enumerate(zip(df_valid_list, df_valid_scaled_list)):
-            for column in target_columns:
-                max_val = scaling_dictionary[column]['max']
-                min_val = scaling_dictionary[column]['min']
-                df_valid_scaled[column] = (df_valid[column].values - min_val) / (max_val - min_val)
-            df_valid_scaled.index = df_valid.index
-            df_valid_scaled_list[i] = df_valid_scaled
-
-    for i, (df_test, df_test_scaled) in enumerate(zip(df_test_list, df_test_scaled_list)):
-            for column in target_columns:
-                max_val = scaling_dictionary[column]['max']
-                min_val = scaling_dictionary[column]['min']
-                df_test_scaled[column] = (df_test[column].values - min_val) / (max_val - min_val)
-            df_test_scaled.index = df_test.index
-            df_test_scaled_list[i] = df_test_scaled
-            
-
-    # Write Scaling factors:
-    with open(scaling_factors_path, 'w') as file:
-        json.dump(scaling_dictionary, file)
-    #Write Test data for further evaluation:
-    df_train_scaled.to_csv(os.path.join(experiment_path,'train_data_scaled.csv'))
-
-    for i in range(len(df_test_scaled_list)):
-        df_test_scaled_list[i].to_csv(os.path.join(experiment_path, f'test{i}_data_scaled.csv'))
-    
-    for i in range(len(df_valid_scaled_list)):
-        df_valid_scaled_list[i].to_csv(os.path.join(experiment_path, f'val{i}_data_scaled.csv'))
 
     sequences_train = generate_sequences(df_train_scaled, lookback, 1)
     print(len(sequences_train))
@@ -148,7 +107,7 @@ def main():
         if valid_loss < best_val_loss:
             best_val_loss = valid_loss
             best_train_loss = train_loss
-            torch.save(model.state_dict(), model_save_path)
+            torch.save(model.state_dict(), os.path.join(model_save_path,f'{target}_best_valid_model.pt'))
     
         df_new_row = { 
                         'train_loss' : train_loss,
@@ -160,28 +119,29 @@ def main():
             }
         lr_tmp = optimizer.param_groups[0]['lr']
         df_metric.loc[epoch] = df_new_row
-        df_metric.to_csv(experiment_path + f'/train_history.csv')
+        df_metric.to_csv(os.path.join(model_save_path,f'{target}_train_history.csv'))
 
         print(f"Epoch {epoch+1}, train_loss={train_loss:0.4g}, val_loss={valid_loss:0.4g}")
     
     training_time = get_time() - start_time
     
-    print('Trainig took: {:.2f}s for {} epochs'.format(training_time, epoch))
+    print('Training took: {:.2f}s for {} epochs'.format(training_time, epoch))
     print('Finish training, Best-Test-Loss: %0.5f, Best-Train-Loss:%0.5f '  % (best_val_loss, best_train_loss))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--device', type=str, default='cuda:1')
+    parser.add_argument('--device', type=str, default='cuda:0')
     parser.add_argument('--batch_size', type=int, default=64, help='total batchsz for train and test')
     parser.add_argument('--epochs', type=int, default=100, help='epoch number')
     parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
     parser.add_argument('--lookback', type=int, default=100, help='look back of network')
     parser.add_argument('--positional_encoding', type = str, default='all', choices=['none', 'sun', 'all'], help='defines which data to use for forecasting')
-    parser.add_argument('--data_path', type=str, default='./raw_data/final_splits')
-    parser.add_argument('--experiment_path', type=str, default='./saved_runs/test_quantiles')
+    parser.add_argument('--experiment_path', type=str, default='./experiments/Rural-LV1-101-2034/BaseScenario/0101-3112', help='path to experiment')
+    parser.add_argument('--target', type=str, default='LV', help='target for training (LV, EC, NP)')
+    
     args = parser.parse_args()
 
     print("device is --------------", args.device)
 
-    main()
+    train_lstm_quantile()
